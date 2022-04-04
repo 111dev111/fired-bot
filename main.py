b6283469835065b2951c465780afd36c93a82db3
111dev111 <atomickuz@gmail.com>
2022-04-03 23:38:07 -0400

    Added Files
    Transferred files from Replit to Github Repo

---
+++ main.py
@@ -0,0 +1,49 @@
+import discord
+import os
+import time
+import discord.ext
+from discord.utils import get
+from discord.ext import commands, tasks
+from discord.ext.commands import has_permissions,  CheckFailure, check
+#^ basic imports for other features of discord.py and python ^
+
+client = discord.Client()
+
+client = commands.Bot(command_prefix = 'f!') #put your own prefix here
+
+@client.event
+async def on_ready():
+    print("Bot Online") #will print "bot online" in the console when the bot is online
+    
+    
+@client.command()
+async def ping(ctx):
+    await ctx.send("pong!") #simple command so that when you type "!ping" the bot will respond with "pong!"
+
+@client.command()
+async def fire(ctx, member : discord.Member):
+  
+ 
+  resp = member.mention + " has been fired." 
+ 
+  await ctx.channel.send(resp)
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+client.run(os.getenv("TOKEN")) 
+
+
+#to keep your bot from shutting down use https://uptimerobot.com then create a https:// monitor and put the link to the website that appewars when you run this repl in the monitor and it will keep your bot alive by pinging the flask server
+#enjoy!
